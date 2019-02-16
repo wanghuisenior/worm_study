4cc6e4d6d2e02d591cd176e64ea412a2e24319bc
wanghui <154306777@qq.com>
2019-02-16 15:39:56 +0800

    init

---
+++ 爬取豆瓣电影top250.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+ @FileName: 爬取豆瓣电影top250.py
+ @Author: 王辉/Administrator
+ @Email: wanghui@zih718.com
+ @Date: 2019-02-16 15:20
+ @Description:
+"""
+
+
+class MovieTop(object):
+	def __init__(self):
+		self.start = 0
+
